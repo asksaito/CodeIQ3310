7a79e396a0761a34369229b6363b28eb14d8fefd
ask saito <es.engineer79@gmail.com>
2017-06-14 22:34:01 +0900

    最終版（1回で成功）

--- CodeIQ3310/Program.cs
+++ CodeIQ3310/Program.cs
@@ -10,6 +10,29 @@ namespace CodeIQ3310
     {
         static void Main(string[] args)
         {
+            var input = new List<string>();
+
+            // 入力データをリストに取得
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                input.Add(line);
+            }
+
+            // 文字列の最大長を取得
+            int maxLength = input.Max((str) => str.Length);
+
+            // パディングした文字列を新しいリストに作成
+            var resultList = new List<string>();
+            input.ForEach((str) =>
+            {
+                int paddingSize = maxLength - str.Length;
+                resultList.Add(new string('_', paddingSize) + str);
+            });
+            
+            // 標準出力に出力
+            string result = String.Join("\n", resultList);
+            Console.WriteLine(result);
         }
     }
 }
